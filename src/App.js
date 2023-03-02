54fb46a16f895635d78b18b281418a209f0ac0ee
vitoraok1 <vitoraoki17@gmail.com>
2023-03-02 16:13:33 -0300

    requisito 01 - configuração inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
+import Login from './pages/Login';
+import Provider from './context/Provider';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Login />
+    </Provider>
   );
 }
 
