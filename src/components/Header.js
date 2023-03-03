7f291afaf5232679e99c0071876772caeb5e63e7
Lennon <lennon_lxs@hotmail.com>
2023-03-03 02:25:45 -0300

    Cria os botões de perfil e busca e o titulo da pagina

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
- 
+  const [pageName, setPageName] = useState('');
+  const history = useHistory();
+
+  return (
+    <div>
+      <button data-testid="profile-top-btn" onClick={ history.push('/profile') }>
+        <img
+          src={ profileIcon }
+          alt="Profile Icon"
+        />
+      </button>
+      <button data-testid="search-top-btn" onClick={ history.push('/profile') }>
+        <img
+          src={ searchIcon }
+          alt="Search Icon"
+        />
+      </button>
+      <h1>{pageName}</h1>
+    </div>
+  );
 }
 
 export default Header;
