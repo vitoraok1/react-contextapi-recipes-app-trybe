c947fa491a12a5e535322bac36cffcca2d0577e2
vitoraok1 <vitoraoki17@gmail.com>
2023-03-04 10:11:06 -0300

    requisito 22 - Implementação do filtro toggle

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import context from '../context/Context';
 import {
@@ -15,23 +15,39 @@ export default function CategoryButton() {
     setMealsData } = useContext(context);
   const maxButton = 5;
   const drinksPage = useRouteMatch('/drinks');
+  const [isMealsFiltered, setIsMealsFiltered] = useState(false);
+  const [isDrinksFiltered, setIsDrinksFiltered] = useState(false);
 
   const fetchFilter = (category) => {
-    if (drinksPage && category) {
+    if (drinksPage && category && isDrinksFiltered) {
       // Precisei usar um .then para esperar a promise se resolver para para de quebrar o código na parte do .slice
+      const everyDrinks = getDrinksData();
+      everyDrinks.then((total) => {
+        setDrinksData(total);
+        setIsDrinksFiltered(!isDrinksFiltered);
+      });
+    } else if (drinksPage && category) {
       const totalPromiseDrinks = getFilterDrinks(category);
       totalPromiseDrinks.then((total) => {
         setDrinksData(total);
+        setIsDrinksFiltered(!isDrinksFiltered);
       });
     } else if (drinksPage && !category) {
       const everyDrinks = getDrinksData();
       everyDrinks.then((total) => {
         setDrinksData(total);
       });
+    } else if (!drinksPage && category && isMealsFiltered) {
+      const everyMeals = getMealsData();
+      everyMeals.then((total) => {
+        setMealsData(total);
+        setIsMealsFiltered(!isMealsFiltered);
+      });
     } else if (!drinksPage && category) {
       const totalPromiseMeals = getFilterMeals(category);
       totalPromiseMeals.then((total) => {
         setMealsData(total);
+        setIsMealsFiltered(!isMealsFiltered);
       });
     } else {
       const everyMeals = getMealsData();
