8534adefd88b68796f7cb11029e399199b6c1584
vitoraok1 <vitoraoki17@gmail.com>
2023-03-04 11:28:26 -0300

    requisito 23 - Alteração button das imagens para Link

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useRouteMatch, useHistory } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 import context from '../context/Context';
 import CategoryButton from './CategoryButton';
 
@@ -7,7 +7,6 @@ export default function Recipes() {
   const { drinksData, mealsData } = useContext(context);
   const maxCards = 12;
   const drinksPage = useRouteMatch('/drinks');
-  const history = useHistory();
 
   const drinksCard = (
     <div>
@@ -15,13 +14,13 @@ export default function Recipes() {
       {drinksData.slice(0, maxCards).map(
         ({ strDrink, strDrinkThumb, idDrink }, index) => (
           <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <button onClick={ () => history.push(`/drinks/${idDrink}`) }>
+            <Link to={ `/drinks/${idDrink}` }>
               <img
                 alt={ strDrink }
                 src={ strDrinkThumb }
                 data-testid={ `${index}-card-img` }
               />
-            </button>
+            </Link>
             <h2 key={ strDrink } data-testid={ `${index}-card-name` }>{ strDrink }</h2>
           </div>
         ),
@@ -34,13 +33,13 @@ export default function Recipes() {
       {mealsData.slice(0, maxCards).map(
         ({ strMeal, strMealThumb, idMeal }, index) => (
           <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <button onClick={ () => history.push(`/meals/${idMeal}`) }>
+            <Link to={ `/meals/${idMeal}` }>
               <img
                 alt={ strMeal }
                 src={ strMealThumb }
                 data-testid={ `${index}-card-img` }
               />
-            </button>
+            </Link>
             <h2 key={ strMeal } data-testid={ `${index}-card-name` }>{ strMeal }</h2>
           </div>
 
