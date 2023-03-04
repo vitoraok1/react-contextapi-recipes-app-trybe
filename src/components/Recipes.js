81625fb0a96e89deb81a1bd4590878c2db05431b
vitoraok1 <vitoraoki17@gmail.com>
2023-03-04 10:19:56 -0300

    requisito 23 - Redirecionamento da pessoa usuária ao clicar no card da receita

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 import context from '../context/Context';
 import CategoryButton from './CategoryButton';
 
@@ -7,18 +7,21 @@ export default function Recipes() {
   const { drinksData, mealsData } = useContext(context);
   const maxCards = 12;
   const drinksPage = useRouteMatch('/drinks');
+  const history = useHistory();
 
   const drinksCard = (
     <div>
       <CategoryButton />
       {drinksData.slice(0, maxCards).map(
-        ({ strDrink, strDrinkThumb }, index) => (
+        ({ strDrink, strDrinkThumb, idDrink }, index) => (
           <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <img
-              alt={ strDrink }
-              src={ strDrinkThumb }
-              data-testid={ `${index}-card-img` }
-            />
+            <button onClick={ () => history.push(`/drinks/${idDrink}`) }>
+              <img
+                alt={ strDrink }
+                src={ strDrinkThumb }
+                data-testid={ `${index}-card-img` }
+              />
+            </button>
             <h2 key={ strDrink } data-testid={ `${index}-card-name` }>{ strDrink }</h2>
           </div>
         ),
@@ -29,13 +32,15 @@ export default function Recipes() {
     <div>
       <CategoryButton />
       {mealsData.slice(0, maxCards).map(
-        ({ strMeal, strMealThumb }, index) => (
+        ({ strMeal, strMealThumb, idMeal }, index) => (
           <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <img
-              alt={ strMeal }
-              src={ strMealThumb }
-              data-testid={ `${index}-card-img` }
-            />
+            <button onClick={ () => history.push(`/meals/${idMeal}`) }>
+              <img
+                alt={ strMeal }
+                src={ strMealThumb }
+                data-testid={ `${index}-card-img` }
+              />
+            </button>
             <h2 key={ strMeal } data-testid={ `${index}-card-name` }>{ strMeal }</h2>
           </div>
 
