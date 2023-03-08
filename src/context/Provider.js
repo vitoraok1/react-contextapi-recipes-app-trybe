886ffe1368ea8de752ad640845fe0ba4eac9df52
Lennon <lennon_lxs@hotmail.com>
2023-03-08 17:13:49 -0300

    Arrumando assincronicidade

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,33 +1,17 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
-import { getRecipes, getRecipesByCategory } from '../services/drinksAndMeals';
 
 function Provider({ children }) {
   const [drinksData, setDrinksData] = useState([]);
   const [mealsData, setMealsData] = useState([]);
   const [drinksCategory, setDrinksCategory] = useState([]);
   const [mealsCategory, setMealsCategory] = useState([]);
-  const [drinkDetails, setDrinkDetails] = useState({});
-  const [mealsDetails, setMealsDetails] = useState({});
-
-  useEffect(() => {
-    const { pathname } = window.location;
-    const type = pathname === '/meals' ? 'themealdb' : 'thecocktaildb';
-
-    const fetchRecipes = async () => {
-      if (pathname === '/drinks') {
-        setDrinksData(await getRecipes(type));
-        setDrinksCategory(await getRecipesByCategory(type));
-      } else {
-        setMealsData(await getRecipes(type));
-        setMealsCategory(await getRecipesByCategory(type));
-      }
-    };
-    fetchRecipes();
-  }, []);
+  const [drinkDetails, setDrinkDetails] = useState([]);
+  const [mealsDetails, setMealsDetails] = useState([]);
 
   // console.log(drinksData);
+  // console.log(mealsData);
 
   const context = useMemo(() => ({
     drinksCategory,
@@ -38,6 +22,8 @@ function Provider({ children }) {
     setMealsData,
     setDrinkDetails,
     setMealsDetails,
+    setDrinksCategory,
+    setMealsCategory,
     drinkDetails,
     mealsDetails,
   }), [drinksCategory, mealsCategory, drinksData, mealsData, mealsDetails, drinkDetails]);
